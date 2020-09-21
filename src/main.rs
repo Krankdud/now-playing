f739205ce3243f881f9e302be99feb8f4ebaa666
charlie <charlieschuy@gmail.com>
2020-09-20 22:53:58 -0700

    Render text

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,36 @@ extern crate sdl2;
 
 use sdl2::event::Event;
 use sdl2::pixels::Color;
+use std::path::Path;
 use std::time::Duration;
 
 fn main() {
     let ctx = sdl2::init().unwrap();
     let video_ctx = ctx.video().unwrap();
+    let ttf_ctx = sdl2::ttf::init().expect("Failed to initialize ttf");
 
-    let window = match video_ctx
+    let font = ttf_ctx
+        .load_font(Path::new("Silver.ttf"), 32)
+        .expect("Failed to load font");
+
+    let window = video_ctx
         .window("rust-sdl2 demo", 800, 600)
         .position_centered()
         .build()
-    {
-        Ok(window) => window,
-        Err(err) => panic!("Failed to create window: {}", err),
-    };
+        .expect("Failed to create window");
 
-    let mut canvas = match window.into_canvas().build() {
-        Ok(canvas) => canvas,
-        Err(err) => panic!("Failed to create renderer: {}", err),
-    };
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("Failed to create canvas");
 
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    let texture_creator = canvas.texture_creator();
+
+    let text = font
+        .render("Hello world")
+        .solid(Color::RGB(255, 255, 255))
+        .expect("Could not render text");
+    let texture = text.as_texture(&texture_creator).unwrap();
 
     let mut event_pump = ctx.event_pump().unwrap();
     'running: loop {
@@ -33,7 +42,10 @@ fn main() {
             }
         }
 
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
         canvas.clear();
+        canvas.set_draw_color(Color::RGB(255, 255, 255));
+        canvas.copy(&texture, None, text.rect()).unwrap();
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
